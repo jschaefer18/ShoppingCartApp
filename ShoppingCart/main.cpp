b55c42cd7ea3b64962803bf6072230785a321732
jschaefer18 <joshschaefer@gmail.com>
2025-06-12 15:37:20 -0600

    Add file reading from store.txt and initial store data

--- ShoppingCart/main.cpp
+++ ShoppingCart/main.cpp
@@ -12,23 +12,44 @@
 #include <cmath>
 #include "Product.h"
 #include <vector>
+#include <fstream>
+#include <sstream>
+
+
 
 using namespace std;
 
+// This function is a for loop that prints out all of the current products of the store.
 void viewStoreItems(const std::vector<Product>& store) {
     std::cout << "\nAvailable Store Items:\n";
     for (const auto& item : store) {
             std::cout << "- " << item.getName() << " - $" << item.getPrice() << "\n";
     }
-    cout << "--------------------\n";
+    cout << "----------------------\n";
 }
 
+
+// This function loads the product data from the store.txt file and stores it in a vector data structure.
+void loadStoreFromFile(std::vector<Product>& store, const std::string& filename) {
+    std::ifstream file(filename);
+    std::string line;
+
+    while (getline(file, line)) {
+        std::stringstream ss(line);
+        std::string name;
+        double price;
+
+        if (getline(ss, name, ',') && ss >> price) {
+            store.push_back(Product(name, price));
+        }
+    }
+
+    file.close();
+}
+// This is the main menu function that will call other functions inside it.
 int main() {
     vector<Product> store;
-
-    store.push_back(Product("Apple", 0.99));
-    store.push_back(Product("Milk", 2.49));
-    store.push_back(Product("Bread", 1.99));
+    loadStoreFromFile(store, "store.txt");
     
     while(true){
         cout << "1: View store items\n"
